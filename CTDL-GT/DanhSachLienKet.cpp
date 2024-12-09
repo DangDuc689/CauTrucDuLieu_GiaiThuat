b34a00a9a20b23b705e418d54034e06f21ecde34
DangDuc689 <folo30000@gmail.com>
2024-12-09 11:20:36 +0700

    update

--- CTDL-GT/DanhSachLienKet.cpp
+++ CTDL-GT/DanhSachLienKet.cpp
@@ -1,29 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct sinhVien{
+typedef struct {
     char name[30];
     char lop[10];
     float gpa;
-};
+} SinhVien;
 
 // node ket thuc bang NULL giong chuoi ky tu
-struct Node{
-    int Data;
-    Node* Next;   // luu dia chi cua node tiep theo
-};
+typedef struct node{ // Cau truc cua node
+    int Key;
+    struct node* pNext;   // luu dia chi cua node tiep theo
+} Node;
 
-Node* createNote(int x){
-    Node* newNode = (Node*) malloc(sizeof(Node));
-    newNode->Data = x;
-    newNode->Next = NULL;
-    return newNode;
-};
+typedef struct {
+    Node *pHead;
+    Node *pTail;
+}List;
 
-void Duyet(Node *Head){ // giống như duyệt qua các phần từ trên mảng
-    while (Head != NULL){
-        printf ("%d ", Head->Data);
-        Head = Head->Next; // giống i++
+void Init(List &l){
+    l.pHead = NULL;
+    l.pTail = NULL;
+}
+
+Node* CreateNote(int x){
+    Node *p = (Node*) malloc(sizeof(Node));
+    if (p == NULL){
+        printf ("Khong cap phat duoc");
+        exit(0);
+    }
+    p->Key = x;
+    p->pNext = NULL;
+    return p;
+}
+
+void InserHead(List &l, Node *p){
+    if (l.pHead == NULL)
+        l.pHead = l.pTail = p; // nếu danh sách rỗng thì p sẽ là nút đầu và cuối luôn
+    else{
+        p->pNext = l.pHead;
+        l.pHead = p;
+    }
+}
+
+void InserTail(List &l, Node *p){
+    if (l.pHead == NULL)
+        l.pHead = l.pTail = p;
+    else{
+        l.pTail->pNext = p;
+        l.pTail = p;
+    }
+}
+
+
+void Duyet(Node *pHead){ // giống như duyệt qua các phần từ trên mảng
+    while (pHead != NULL){
+        printf ("%d ", pHead->Key);
+        pHead = pHead->pNext; // giống i++
     }
 }
 
@@ -31,13 +64,13 @@ int Count(Node *Head){ // đếm các phần tử, như mảng
     int dem = 0;
     while (Head != NULL){
         dem++;
-        Head = Head->Next;
+        Head = Head->pNext;
     }
     return dem;
 }
 
 int main(){
-
+    List l;
 
     return 0;
 }
---
+++ CTDL-GT/test.cpp
@@ -0,0 +1,180 @@
+#include <string.h>
+void InterchangeSort(int a[], int n){
+    for (int i = 0; i < n-1; i++){
+        for (int j = i+1; j < n; j++){
+            if (a[i] > a[j]){
+                swap(a[i], a[j]);
+            }
+        }
+    }
+}
+
+5
+2 3 4 5 9
+      
+
+void InsertionSort(int a[], int n){
+    for (int i = 1; i < n; i++){
+        int x = a[i];
+        int j = i-1;
+        while (j >= 0 && a[j] > x){
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = x;
+    }
+}
+
+
+                x=7                   
+        2   4   7   6   10
+            j   i          
+// -1   0   1   2   3   4     (thu tu)
+
+void BubbleSort(int a[], int n){
+    for (int i = 0; i < n; i++){
+        for (j = n-1; j > i; j--){
+            if (a[j] < a[j-1]){
+                swap(a[j], a[j-1]);
+            }
+        }
+    }
+}
+      
+
+        0   3   7   5   9  
+                i       j
+                    j-1
+    //  0   1   2   3   4
+
+
+void SelectionSort(int a[], int n){
+    for (int i = 0; i < n-1; i++){
+        int x = i;
+        for (int j = i+1; j < n; j++){
+            if (a[j] < a[x])
+                x = j;
+        }
+        swap(a[x], x[i]);
+    }
+}
+
+
+        2   3   8   9   4
+                i   j                    
+                x           
+    //  0   1   2   3   4
+
+
+void QuickSort(int a[], int left, int right){
+    int x = a[(left + right) / 2]
+    int i = left;
+    int j = right;
+
+    do{
+        while (a[i] < x) i++;
+        while (a[j] > x) j--;
+        if (i <= j){
+            swap(a[i], a[j]);
+            i++;
+            j--;
+        }
+    } while(i < j);
+
+    if (left < j) QuickSort(a, left, j);
+    if (i < right) QuickSort(a, i, right);
+}
+
+                    x=2
+        2   9   3   5   4   7   8
+        j   i   
+    //  0   1   2   3   4   5   6
+
+            left
+            2               
+
+            right
+                    x=5
+            4   3   5   9   7   8
+                j       i
+        //  0   1   2   3   4   5
+
+                left 
+                x=4
+                3   4
+                j   i
+            //  0   1
+
+                right
+                    x=9
+                5   8   7   9
+                        j   i
+            //  0   1   2   3   
+
+                    left
+                        x=8  
+                    5   7   8
+                        j   i
+                //  0   1   2
+        
+
+void InterchangeSort(int a[], int n){
+    for (int i = 0; i < n-1; i++){
+        for (int j = i+1; j < n; j++){
+            if (a[i] > a[j])
+                swap(a[i], a[j]);
+        }
+    }
+}
+
+void BubbleSort(int a[], int n){
+    for (int i = 0; i < n; i++){
+        for (int j = n-1; j > i; j--){
+            if (a[j] < a[j-1])
+                swap(a[j], a[j-1]);
+        }
+    }
+}
+
+void SelectionSort(int a[], int n){
+    for (int i = 0; i < n-1; i++){
+        int x = i;
+        for (int j = i+1; j < n; j++){
+            if (a[j] < a[x])
+                x = j;
+        }
+        swap(a[i], a[x]);
+    }
+}
+
+void InsertionSort(char s[]){
+    int n = strlen(s);
+    for (int i = 1; i < n; i++){
+        int x = a[i];
+        int j = i-1;
+        while (j >= 0 && a[j] > x){
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = x;
+    }
+}
+
+void QuickSort(int a[], int left, int right){
+    int x = a[(left + right) / 2];
+    int i = left;
+    int j = right;
+
+    do{
+        while (a[i] < x) i++;
+        while (a[j] > x) j--;
+        if (i <= j){
+            swap(a[i], a[j]);
+            i++;
+            j--;
+        }
+    } while(i < j);
+
+    if (left < j) QuickSort(a, left, j);
+    if (i < right) QuickSort(a, i, right);
+}
